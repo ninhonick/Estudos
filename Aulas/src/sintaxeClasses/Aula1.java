7de44df8d61e57d1e2808150a231c051ceff9607
ninhonick <ninhosvs@gmail.com>
2023-03-28 09:43:54 -0300

    iniciando curso na dio com versionamento

---
+++ Aulas/src/sintaxeClasses/Aula1.java
@@ -0,0 +1,26 @@
+package sintaxeClasses;
+
+import java.util.Scanner;
+
+public class Aula1 {
+   public static void main(String[] args) {
+	System.out.println("vamos começar meus nobres!!!! ");
+	
+	Scanner tcl = new Scanner (System.in);
+	
+	System.out.println("digite seu nome!");	
+	String nome = tcl.next();
+	System.out.println("digite seu sobrenome!");
+	String sobreNome = tcl.next();
+	
+	String nomeComplet = nomeCompleto(nome,sobreNome);
+	
+	System.out.println(nomeComplet);
+	
+	
+}
+
+ public static String nomeCompleto (String nome1, String nome2) {
+	return nome1.concat(" ").concat(nome2);
+}
+}
