a1514997f32f91dc846779b7eb43338a14471c8e
ninhonick <ninhosvs@gmail.com>
2023-03-30 23:48:41 -0300

    continuação dos estudos

---
+++ Aulas/src/sintaxeClasses/AboutMe.java
@@ -0,0 +1,10 @@
+package sintaxeClasses;
+
+public class AboutMe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Aulas/src/sintaxeClasses/testeCmd.java
@@ -0,0 +1,10 @@
+package sintaxeClasses;
+
+public class testeCmd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
