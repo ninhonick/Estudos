4eeb6334280099010977879a1e1514d8562e6559
ninhonick <ninhosvs@gmail.com>
2023-04-07 11:08:59 -0300

    aula sobre loop

---
+++ Aulas/src/sintaxeClasses/exericioLop.java
@@ -0,0 +1,37 @@
+package sintaxeClasses;
+
+import java.util.Scanner;
+
+public class exericioLop {
+
+	public static void main(String[] args) {
+		
+		Scanner tlc = new Scanner (System.in);
+		
+		String nome;
+		int idade ;
+		
+		
+		while (true) {
+			System.out.println("Qual é seu nome?");
+			
+			nome = tlc.next();
+			
+			if (nome.equals("0")) break;
+			
+		
+			System.out.println("Qual é sua idade?");
+			 idade = tlc.nextInt(); 
+			
+			System.out.println(nome + " tem "  + idade + " anos");
+			
+				
+		
+		}
+		tlc.close();
+		
+		System.out.println("Acabou abestado");
+	
+	}
+
+}
