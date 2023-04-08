a55b495d1aa9ec3da520bf12ce705913230c9720
ninhonick <ninhosvs@gmail.com>
2023-04-08 19:22:15 -0300

    testando

--- Aulas/src/sintaxeClasses/looops.java
+++ Aulas/src/sintaxeClasses/looops.java
@@ -6,15 +6,31 @@ public class looops {
 
 	public static void main(String[] args) {
 		Scanner tcl = new Scanner (System.in);
-		int nota;
+		int nota = 0, qtNotas = 0,count = 0;
 		 
-		System.out.println("informe uma nota: ");
-		nota = tcl.nextInt();
-		while (nota < 0 | nota >10) {
-			System.out.println("nota invalida.Digite novamente!!");
-			
+		
+		 
+		System.out.println("Quantas notas obtidas?: ");
+		qtNotas = tcl.nextInt();
+		
+		while(count < qtNotas) {
+			System.out.println("informe uma nota: ");
 			nota = tcl.nextInt();
+			if (nota <0 || nota > 10 ) {
+			 System.out.println("nota invalida.Digite novamente!!");
+			 nota = tcl.nextInt();
+			 
+			 count ++
+			 
+			}
+			 
 		}
+		
+		
+		
+			
+			
+		
 		System.out.println("parabens você tem ---> " + nota + " como nota atribuida");
 		
 		
